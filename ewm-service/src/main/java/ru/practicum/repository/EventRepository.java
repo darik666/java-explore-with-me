9cbe2f7c2e26016c2aa4b941998aa81f5d774e87
darik666 <tattsar666@yandex.ru>
2023-07-22 21:15:48 +0300

    TestPush

--- ewm-service/src/main/java/ru/practicum/repository/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/repository/EventRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.EventStatus;
-import ru.practicum.dto.EventShortDto;
 import ru.practicum.model.Event;
 
 import java.time.LocalDateTime;
