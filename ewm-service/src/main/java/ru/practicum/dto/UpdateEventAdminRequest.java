d6937c03761b88cb16ee643f238fb0a28e8e67c8
darik666 <tattsar666@yandex.ru>
2023-07-23 20:42:05 +0300

    TestPush

--- ewm-service/src/main/java/ru/practicum/dto/UpdateEventAdminRequest.java
+++ ewm-service/src/main/java/ru/practicum/dto/UpdateEventAdminRequest.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.practicum.ActionEnum;
-import ru.practicum.State;
 
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
